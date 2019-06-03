d6f342ec737025086487d5d06cd179e7a33b1117
Marc Farra <marcfarra@gmail.com>
2019-06-03 16:24:14 -0400

    override user profile method

--- app/lib/osm.js
+++ app/lib/osm.js
@@ -11,11 +11,36 @@ const OSMStrategy = require('passport-openstreetmap').Strategy
 
 const { serverRuntimeConfig, publicRuntimeConfig } = require('../../next.config')
 
+
 // get an authentication token pair from openstreetmap
 function openstreetmap (req, res) {
   const { OSM_CONSUMER_KEY, OSM_CONSUMER_SECRET, OSM_API, OSM_DOMAIN } = serverRuntimeConfig
   const query = url.parse(req.url, true).query
   const challenge = query.login_challenge
+
+  /**
+   * override the userProfile method of OSMStrategy to allow for custom osm endpoints
+   */
+  OSMStrategy.prototype.userProfile = function (token, tokenSecret, params, done) {
+    this._oauth.get(`${OSM_API}/api/0.6/user/details`, token, tokenSecret, function (err, body, res) {
+      if (err) { return done(new InternalOAuthError('failed to fetch user profile', err)) }
+
+      var parser = new xml2js.Parser()
+      parser.parseString(body, function (err, xml) {
+        if (err) { return done(err) };
+
+        var profile = { provider: 'openstreetmap' }
+        profile.id = xml.user['@'].id
+        profile.displayName = xml.user['@'].display_name
+
+        profile._raw = body
+        profile._xml2json =
+          profile._xml2js = xml
+
+        done(null, profile)
+      })
+    })
+  }
 
   const strategy = new OSMStrategy({
     requestTokenURL: `${OSM_API}/oauth/request_token`,
