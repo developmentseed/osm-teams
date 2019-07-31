c9b354bf785cd68daea008e961bb8a75fa7b5fca
Marc Farra <marcfarra@gmail.com>
2019-07-30 20:38:35 -0400

    allow disable color for button

--- components/button.js
+++ components/button.js
@@ -3,17 +3,28 @@ import getConfig from 'next/config'
 import join from 'url-join'
 const { publicRuntimeConfig } = getConfig()
 
-export default function Button ({ href, onClick, children, danger, small }) {
+export default function Button ({ href, onClick, children, danger, small, disabled }) {
   let color = 'dark-green'
   let size = 'bw2 ph3 pv2 mb2'
+
   if (danger) {
     color = 'near-black'
   }
+
+  if (disabled) {
+    color = 'near-gray'
+  }
+
   if (small) {
     size = 'bw1 ph2 pv1 mb1'
   }
 
   const commonStyle = `${color} ${size} f6 link dim br1 ba dib pointer`
+
+  if (disabled) {
+    return <div className={commonStyle} >{children}</div>
+  }
+
   if (href) {
     const fullUrl = join(publicRuntimeConfig.APP_URL, href)
     return <a href={fullUrl} className={`link ${commonStyle}`}>{children}</a>
