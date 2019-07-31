f02e438115e0c82af45689eb3fd24ffe511a88fb
Marc Farra <marcfarra@gmail.com>
2019-07-30 20:39:21 -0400

    add migration with postgis

---
+++ app/db/migrations/20190730183820_team-locations.js
@@ -0,0 +1,18 @@
+
+exports.up = async function (knex) {
+  try {
+    await knex.raw('CREATE EXTENSION IF NOT EXISTS "postgis"')
+    await knex.raw('ALTER TABLE team ADD COLUMN location geometry(POINT, 4326)')
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+exports.down = async function (knex) {
+  try {
+    await knex.raw('ALTER TABLE team DROP COLUMN location')
+    await knex.schema.raw('DROP EXTENSION "postgis"')
+  } catch (e) {
+    console.error(e)
+  }
+}
