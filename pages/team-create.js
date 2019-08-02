0649dec10e950ee9ab6b68b39f0ac33ab9c9db7a
sethvincent <sethvincent@gmail.com>
2019-08-02 15:54:16 -0400

    create team page

---
+++ pages/team-create.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import join from 'url-join'
+import Router from 'next/router'
+import { createTeam } from '../lib/teams-api'
+import getConfig from 'next/config'
+import EditTeamForm from '../components/edit-team-form'
+const { publicRuntimeConfig } = getConfig()
+
+export default class TeamCreate extends Component {
+  render () {
+    return (
+      <article>
+        <EditTeamForm
+          onSubmit={async (values, actions) => {
+            console.log('values', values)
+            try {
+              const team = await createTeam(values)
+              actions.setSubmitting(false)
+              Router.push(join(publicRuntimeConfig.APP_URL, `/teams/${team.id}`))
+            } catch (e) {
+              console.error(e)
+              actions.setSubmitting(false)
+              // set the form errors actions.setErrors(e)
+              actions.setStatus(e.message)
+            }
+          }}
+        />
+      </article>
+    )
+  }
+}
--- pages/team-edit.js
+++ pages/team-edit.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react'
 import join from 'url-join'
 import Router from 'next/router'
 import { pick } from 'ramda'
-import { Formik, Field, Form, ErrorMessage } from 'formik'
 import { getTeam, updateTeam } from '../lib/teams-api'
 import getConfig from 'next/config'
-import Button from '../components/button'
-import { FormikMap } from '../components/formikmap'
+import EditTeamForm from '../components/edit-team-form'
+
 const { publicRuntimeConfig } = getConfig()
 
 export default class Team extends Component {
@@ -67,7 +66,7 @@ export default class Team extends Component {
 
     return (
       <article>
-        <Formik
+        <EditTeamForm
           initialValues={pick(['name', 'bio', 'hashtag', 'location'], team)}
           onSubmit={async (values, actions) => {
             try {
@@ -81,32 +80,6 @@ export default class Team extends Component {
               actions.setStatus(e.message)
             }
           }}
-          render={({ status, isSubmitting, submitForm, values, setFieldValue }) => (
-            <Form>
-              <div className='mt3'>
-                <label htmlFor='name' className='db fw4 lh-copy f6'>Name:</label>
-                <Field type='text' name='name' />
-                <ErrorMessage name='name' component='div' />
-              </div>
-              <div className='mt3'>
-                <label htmlFor='hashtag' className='db fw4 lh-copy f6'>Hashtag:</label>
-                <Field type='text' name='hashtag' />
-                <ErrorMessage name='hashtag' component='div' />
-              </div>
-              <div className='mt3'>
-                <label htmlFor='bio' className='db fw4 lh-copy f6'>Description:</label>
-                <Field type='textarea' name='bio' />
-                <ErrorMessage name='bio' component='div' />
-              </div>
-              <div className='mt3'>
-                <FormikMap name='location' value={values.location} onChange={setFieldValue} />
-              </div>
-              <div className='mt3'>
-                { status && status.msg && <div>{status.msg}</div> }
-                <Button disabled={isSubmitting} onClick={() => submitForm()}>Submit</Button>
-              </div>
-            </Form>
-          )}
         />
       </article>
     )
