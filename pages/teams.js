20e0c77ca57df0356b3ccc99fae10ce2902d8d29
Marc Farra <marcfarra@gmail.com>
2019-08-01 20:33:52 -0400

    [wip] map on list page

--- pages/teams.js
+++ pages/teams.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react'
 import getConfig from 'next/config'
+import Map from 'pigeon-maps'
+import Marker from '../components/marker'
 import Router from 'next/router'
 import Section from '../components/section'
 import Table from '../components/table'
 import join from 'url-join'
+import { isNil, pick, reject, map, props, prop } from 'ramda'
 import { getTeams } from '../lib/teams-api'
 
 const { publicRuntimeConfig } = getConfig()
 const URL = publicRuntimeConfig.APP_URL
 
 export default class TeamList extends Component {
-  static async getInitialProps ({ query }) {}
+  static async getInitialProps () {}
 
   constructor (props) {
     super(props)
     this.state = {
       loading: true,
-      error: undefined
+      error: undefined,
+      teams: [],
+      map: { center: [30, 30], zoom: 1 }
     }
   }
 
-  async componentDidMount () {
+  async getTeams () {
     try {
-      const teams = await getTeams()
+      // Filters
+      const bbox = this.state.mapBounds
+
+      const teams = await getTeams({ bbox })
       this.setState({
         teams,
         loading: false
@@ -35,6 +43,10 @@ export default class TeamList extends Component {
         loading: false
       })
     }
+  }
+
+  async componentDidMount () {
+    this.getTeams()
   }
 
   renderTeams () {
@@ -60,10 +72,43 @@ export default class TeamList extends Component {
     )
   }
 
+  renderMap () {
+    const DivMarker = ({ left, top, style, children }) => (
+      <div style={{
+        position: 'absolute',
+        left: left,
+        top: top,
+        style: {
+          width: 30,
+          height: 30,
+          background: 'red',
+          borderBottomLeftRadius: '100%',
+          borderBottomRightRadius: '100%'
+        }
+      }} >{children}</div>
+    )
+    const { teams, map: { center, zoom } } = this.state
+    if (!teams) return null
+    if (teams.length === 0) {
+      return <div />
+    }
+
+    const teamLocations = map(pick(['location', 'id']), teams)
+    const locations = teamLocations.filter(({ location }) => !!location) // reject nulls
+    const centers = map(({ location, id }) => ({ id, center: JSON.parse(location).coordinates.reverse() }), locations)
+
+    return (
+      <Map center={center} mouseEvents zoom={zoom} width={400} height={300} >
+        {centers.map(c => <Marker key={c.id} anchor={c.center} payload={c.id} />) }
+      </Map>
+    )
+  }
+
   render () {
     return (
       <div>
         <h2>Teams</h2>
+        { this.renderMap() }
         <Section>
           {this.renderTeams()}
         </Section>
