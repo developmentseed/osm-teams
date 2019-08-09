04eca7a8546177f985bdb9bd65c3ab4cb594f6c8
Lane Goodman <Lane@developmentseed.org>
2019-08-09 00:18:26 -0400

    Lint

--- components/edit-team-form.js
+++ components/edit-team-form.js
@@ -58,8 +58,8 @@ export default function EditTeamForm ({ initialValues, onSubmit }) {
               `}
             </style>
           </Form>
-          )
-        }
+        )
+      }
       }
     />
   )
