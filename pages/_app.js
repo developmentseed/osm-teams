685625d367e6d9fe750276ebfd87a7dccb784716
sethvincent <sethvincent@gmail.com>
2019-08-15 18:14:18 -0700

    lintfix

--- pages/_app.js
+++ pages/_app.js
@@ -54,7 +54,7 @@ class OSMHydra extends App {
           <link rel='stylesheet' href='https://unpkg.com/leaflet@1.5.1/dist/leaflet.css'
             integrity='sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=='
             crossOrigin='' />
-          <link rel="stylesheet" href="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css" />
+          <link rel='stylesheet' href='https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css' />
           <link rel='shortcut icon' href='/static/favicon.ico' />
           <link rel='icon' type='image/png' href='/static/favicon.png' />
         </Head>
