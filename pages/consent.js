7b85494b952af27e804982746bf96ed06cdc497a
Marc Farra <marcfarra@gmail.com>
2019-08-27 18:44:58 +0300

    Fix consent page buttons

--- pages/consent.js
+++ pages/consent.js
@@ -72,8 +72,8 @@ class Consent extends Component {
             <label htmlFor='remember'>Do not ask me again</label>
           </p>
           <p>
-            <Button type='submit' id='accept' name='submit' value='Allow access' />
-            <Button type='submit' id='reject' name='submit' value='Deny access' />
+            <Button type='submit' id='accept' name='submit' value='Allow access'>Allow Access</Button>
+            <Button type='submit' id='reject' name='submit' value='Deny access'>Deny Access</Button>
           </p>
         </form>
         <style jsx>
