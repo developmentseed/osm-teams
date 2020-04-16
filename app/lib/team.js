developmentseed/osm-teams

commit 0205ea32bee88635d82e1aecc4d5dfca45b62f10

Add member and moderator ids to listTeams response. (#173)
* Add member and moderator ids to listTeams response.

* Upgrade ava to latest.

* JsDoc improvement.

* Re-use ramda prop().

* Unit test for team.listMembers() and team.listModerators().

* Add moderator ids and member ids to getOrgTeams().

* Update OpenAPI spec.

* Unit tests in organization-api and team-api.

app/lib/team.js
@@ -113,6 +113,34 @@ async function list (options) {
 }
 
 /**
+ * List the member osm ids for a list of team ids.
+ *
+ * @param {number[]} teamIds
+ * @returns {Promise<*>}
+ * @async
+ */
+async function listMembers (teamIds) {
+  const conn = await db()
+  return conn('member').whereIn('team_id', teamIds)
+    .join('users', 'member.osm_id', 'users.id')
+    .select('team_id', 'osm_id')
+}
+
+/**
+ * List the moderator osm ids for a list of team ids.
+ *
+ * @param {number[]} teamIds
+ * @returns {Promise<*>}
+ * @async
+ */
+async function listModerators (teamIds) {
+  const conn = await db()
+  return conn('moderator').whereIn('team_id', teamIds)
+    .join('users', 'moderator.osm_id', 'users.id')
+    .select('team_id', 'osm_id')
+}
+
+/**
  * List all the teams which osm id is a moderator of.
  *
  * @param {*} osmId osm user id to filter by
@@ -134,7 +162,7 @@ async function listModeratedBy (osmId) {
 *
 * @param {object} data - params for a team
 * @param {string} data.name - name of the team
-* @param {geojson} data.location - lat/lon of team
+* @param {geojson?} data.location - lat/lon of team
 * @param {int} osmId - id of first moderator
 * @param {object=} trx - optional parameter for database connection to re-use connection in case of nested
 *   transactions. This is used when a team is created as part of an organization
@@ -349,6 +377,8 @@ async function associatedOrg (teamId) {
 module.exports = {
   get,
   list,
+  listMembers,
+  listModerators,
   listModeratedBy,
   create,
   update,

app/manage/organizations.js
@@ -1,5 +1,6 @@
 const organization = require('../lib/organization')
 const team = require('../lib/team')
+const { teamsMembersModeratorsHelper } = require('./utils')
 
 /**
  * Create an organization
@@ -200,7 +201,8 @@ async function getOrgTeams (req, reply) {
   const { id } = req.params
   try {
     const data = await team.list({ organizationId: id })
-    reply.send(data)
+    const enhancedData = await teamsMembersModeratorsHelper(data)
+    reply.send(enhancedData)
   } catch (err) {
     console.log(err)
     return reply.boom.badRequest(err.message)

app/manage/teams.js
@@ -1,8 +1,10 @@
 const team = require('../lib/team')
 const { prop, map } = require('ramda')
 const urlRegex = require('url-regex')
+const { teamsMembersModeratorsHelper } = require('./utils')
 
 const isUrl = urlRegex({ exact: true })
+const getOsmId = prop('osm_id')
 
 async function listTeams (req, reply) {
   const { osmId, bbox } = req.query
@@ -16,7 +18,8 @@ async function listTeams (req, reply) {
 
   try {
     const data = await team.list({ osmId, bbox: bounds })
-    reply.send(data)
+    const enhancedData = await teamsMembersModeratorsHelper(data)
+    reply.send(enhancedData)
   } catch (err) {
     console.log(err)
     return reply.boom.badRequest(err.message)
@@ -48,7 +51,7 @@ async function getTeam (req, reply) {
 
   try {
     const teamData = await team.get(id)
-    const memberIds = map(prop('osm_id'), (await team.getMembers(id)))
+    const memberIds = map(getOsmId, (await team.getMembers(id)))
     const members = await team.resolveMemberNames(memberIds)
     const moderators = await team.getModerators(id)
 

app/manage/utils.js
@@ -1,0 +1,33 @@
+const team = require('../lib/team')
+const { prop } = require('ramda')
+
+const getId = prop('id')
+const getOsmId = prop('osm_id')
+
+/**
+ * Helper function for teams and organizations. Enhances teamsData with list of
+ * moderator ids and list of member ids.
+ *
+ * @param {Object[]} teamsData
+ * @returns {Promise<*>}
+ * @async
+ */
+async function teamsMembersModeratorsHelper (teamsData) {
+  const teamIds = teamsData.map(getId)
+  const [members, moderators] = await Promise.all([
+    team.listMembers(teamIds),
+    team.listModerators(teamIds)
+  ])
+  return teamsData.map(team => {
+    const predicate = ({ team_id }) => team_id === team.id
+    return {
+      ...team,
+      members: members.filter(predicate).map(getOsmId),
+      moderators: moderators.filter(predicate).map(getOsmId)
+    }
+  })
+}
+
+module.exports = {
+  teamsMembersModeratorsHelper
+}

app/tests/api/organization-api.test.js
@@ -209,4 +209,10 @@ test('get org teams', async t => {
 
   const orgTeams = await agent.get(`/api/organizations/${res.body.id}/teams`)
   t.is(orgTeams.body.length, 2)
+  orgTeams.body.forEach(item => {
+    t.truthy(item.name)
+    t.truthy(item.id)
+    t.truthy(item.members.length)
+    t.truthy(item.moderators.length)
+  })
 })

app/tests/api/team-api.test.js
@@ -124,6 +124,8 @@ test('get team list', async t => {
   teams.body.forEach(item => {
     t.truthy(item.name)
     t.truthy(item.id)
+    t.truthy(item.members.length)
+    t.truthy(item.moderators.length)
   })
 })
 

app/tests/api/team-model.test.js
@@ -2,6 +2,7 @@ const path = require('path')
 const test = require('ava')
 const db = require('../../db')
 const team = require('../../lib/team')
+const { prop } = require('ramda')
 
 const migrationsDirectory = path.join(__dirname, '..', '..', 'db', 'migrations')
 
@@ -55,6 +56,32 @@ test('list teams', async (t) => {
     t.truthy(item.name)
     t.truthy(item.id)
   })
+})
+
+const getOsmId = prop('osm_id')
+
+test('list team(s) members', async (t) => {
+  const team1 = await team.create({ name: 'list team members test' }, 1)
+  const team2 = await team.create({ name: 'list team members test2' }, 1)
+  await team.addMember(team2.id, 2)
+  const list = await team.listMembers([team1.id, team2.id])
+  const team1Members = list.filter(({ team_id }) => team_id === team1.id).map(getOsmId)
+  const team2Members = list.filter(({ team_id }) => team_id === team2.id).map(getOsmId)
+  t.deepEqual(team1Members, [1])
+  t.deepEqual(team2Members, [1, 2])
+})
+
+test('list team(s) moderators', async (t) => {
+  const team1 = await team.create({ name: 'list team mods test' }, 1)
+  const team2 = await team.create({ name: 'list team mods test2' }, 1)
+  await team.addMember(team2.id, 2)
+  await team.assignModerator(team2.id, 2)
+  await team.removeModerator(team2.id, 1)
+  const list = await team.listModerators([team1.id, team2.id])
+  const team1Mods = list.filter(({ team_id }) => team_id === team1.id).map(getOsmId)
+  const team2Mods = list.filter(({ team_id }) => team_id === team2.id).map(getOsmId)
+  t.deepEqual(team1Mods, [1])
+  t.deepEqual(team2Mods, [2])
 })
 
 test('update a team', async (t) => {

docs/api.yml
@@ -55,6 +55,16 @@ components:
           type: string
           format: uri
           description: link to organized editing policy of the team
+        members:
+          type: array
+          description: list of team member's osm id
+          items:
+            type: number
+        moderators:
+          type: array
+          description: list of team member's osm id
+          items:
+            type: number
     TeamList:
       type: array
       items:
@@ -503,7 +513,7 @@ paths:
         moderator may exist concurrently. Moderators are listed in the
         TeamModeratorList schema.
       tags:
-        - teams      
+        - teams
       responses:
         '200':
           description: member was promoted to moderator
@@ -522,7 +532,7 @@ paths:
         Remove/Demote a moderator of a team. At least one moderator must exist
         for a team. Moderators are listed in the TeamModeratorList schema.
       tags:
-        - teams      
+        - teams
       responses:
         '200':
           description: member was demoted from moderator
@@ -615,7 +625,7 @@ paths:
         Assign/Promote a user to be an owner of an organization. More than one
         owner may exist concurrently. Owners can manage organizations of an organization.
       tags:
-        - organizations      
+        - organizations
       responses:
         '200':
           description: user is promoted to owner of organization
@@ -634,7 +644,7 @@ paths:
         Remove/Demote an owner of an organization. At least one owner
         must remain in the organization.
       tags:
-        - organizations      
+        - organizations
       responses:
         '200':
           description: user is demoted from owner
@@ -654,7 +664,7 @@ paths:
         manager may exist concurrently. Managers can create organizations for an organization
         but cannot update the organization.
       tags:
-        - organizations      
+        - organizations
       responses:
         '200':
           description: user is promoted to manager of organization
@@ -672,7 +682,7 @@ paths:
       summary: >
         Remove/Demote manager of an organization. An org can have no managers.
       tags:
-        - organizations      
+        - organizations
       responses:
         '200':
           description: user is demoted from manager of organization

commit 20bcb4b6312e4a3e51f6d0b57cb8cae827743e0e

Fix members and moderators in listTeams response.
- Fixes a bug caused by #173
- listMembers() and listModerators() should not join with users table
  (they are just osm users, but probably not app users)

app/lib/team.js
@@ -121,9 +121,9 @@ async function list (options) {
  */
 async function listMembers (teamIds) {
   const conn = await db()
-  return conn('member').whereIn('team_id', teamIds)
-    .join('users', 'member.osm_id', 'users.id')
+  return conn('member')
     .select('team_id', 'osm_id')
+    .whereIn('team_id', teamIds)
 }
 
 /**
@@ -135,9 +135,9 @@ async function listMembers (teamIds) {
  */
 async function listModerators (teamIds) {
   const conn = await db()
-  return conn('moderator').whereIn('team_id', teamIds)
-    .join('users', 'moderator.osm_id', 'users.id')
+  return conn('moderator')
     .select('team_id', 'osm_id')
+    .whereIn('team_id', teamIds)
 }
 
 /**