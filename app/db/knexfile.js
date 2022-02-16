e1e591e5ce682b5d893b6ce2d5601deb06940669
Vitor George <vitor.george@gmail.com>
2022-02-16 17:54:44 +0000

    Always apply process.env.DATABASE_URL is present

--- app/db/knexfile.js
+++ app/db/knexfile.js
@@ -1,13 +1,13 @@
-let DATABASE_URL
+let DATABASE_URL = process.env.DATABASE_URL
 
-if (!process.env.NODE_ENV) {
-  process.env.NODE_ENV = 'development'
-}
-
-if (process.env.NODE_ENV === 'development') {
-  DATABASE_URL = 'postgres://postgres:postgres@localhost:5433/osm-teams?sslmode=disable'
-} else if (process.env.NODE_ENV === 'test') {
-  DATABASE_URL = 'postgres://postgres:postgres@localhost:5434/osm-teams?sslmode=disable'
+if (!DATABASE_URL) {
+  if (process.env.NODE_ENV === 'development') {
+    DATABASE_URL =
+      'postgres://postgres:postgres@localhost:5433/osm-teams?sslmode=disable'
+  } else if (process.env.NODE_ENV === 'test') {
+    DATABASE_URL =
+      'postgres://postgres:postgres@localhost:5434/osm-teams-test?sslmode=disable'
+  }
 }
 
 module.exports = {
