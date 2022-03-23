af8a2835162581c4caaccb5dcb1e6476bd455654
Marc Farra <marcfarra@gmail.com>
2022-03-23 15:19:54 +0200

    Add attribute validation to form

--- components/profile-form.js
+++ components/profile-form.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import * as Yup from 'yup'
 import Router from 'next/router'
 import descriptionPopup from './description-popup'
-import { Formik, Field, Form } from 'formik'
+import { Formik, Field, Form, ErrorMessage } from 'formik'
 import { getOrgMemberAttributes, getTeamMemberAttributes, getMyProfile, setMyProfile } from '../lib/profiles-api'
 import { getTeam } from '../lib/teams-api'
 import Button from '../components/button'
-import { prop } from 'ramda'
+import { prop, propOr } from 'ramda'
 export default class ProfileForm extends Component {
   static async getInitialProps ({ query }) {
     if (query) {
@@ -59,16 +60,66 @@ export default class ProfileForm extends Component {
     }
   }
 
+
   render () {
-    let { memberAttributes, orgAttributes, profileValues, returnUrl } = this.state
-    profileValues = profileValues || {}
+    let { memberAttributes, orgAttributes, profileValues, returnUrl, loading } = this.state
+
+    if (loading) {
+      return <div>Loading...</div>
+    }
+
+    const allAttributes = memberAttributes.concat(orgAttributes)
+    let initialValues = {}
+
+    let schema = {}
+
+    allAttributes.forEach(attr => {
+      // Set initial value from profileValues or to empty string
+      initialValues[attr.id] = propOr('', attr.id, profileValues)
+
+      // Set form validator
+      switch (attr.key_type) {
+        case 'email': {
+          schema[attr.id] = Yup.string().email('Invalid email')
+          break;
+        }
+        case 'number': {
+          schema[attr.id] = Yup.number().typeError('Invalid number')
+          break;
+        }
+        case 'url': {
+          schema[attr.id] = Yup.string().url('Invalid URL')
+          break;
+        }
+        case 'date': {
+          schema[attr.id] = Yup.date('Invalid date')
+          break;
+        }
+        case 'tel': {
+          const phoneRegex = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+          schema[attr.id] = Yup.string().matches(phoneRegex, 'Invalid phone number')
+          break;
+        }
+        case 'color': {
+          const hexRegex = /^#([0-9a-f]{3}|[0-9a-f]{6})$/i
+          schema[attr.id] = Yup.string().matches(hexRegex, 'Invalid color code')
+          break
+        }
+        default: {
+          schema[attr.id] = Yup.string()
+        }
+      }
+    })
+    const yupSchema = Yup.object().shape(schema)
 
     return (
       <article className='inner page'>
         <h1>Add Your Profile</h1>
         <Formik
           enableReinitialize
-          initialValues={profileValues}
+          validateOnBlur
+          validationSchema={yupSchema}
+          initialValues={initialValues}
           onSubmit={async (values, actions) => {
             const data = Object.keys(values).map(key => ({
               'key_id': key,
@@ -99,11 +150,13 @@ export default class ProfileForm extends Component {
                           {attribute.description ? descriptionPopup(attribute.description) : ''}
                         </label>
                         <Field
-                          type='text'
+                          type={attribute.key_type}
                           name={attribute.id}
                           required={attribute.required}
-                          value={prop(attribute.id, values)}
                         />
+                        <div className='form--error'>
+                          <ErrorMessage name={attribute.id} />
+                        </div>
                       </div>
                     })}
                   </>
@@ -117,11 +170,13 @@ export default class ProfileForm extends Component {
                       {attribute.description ? descriptionPopup(attribute.description) : ''}
                     </label>
                     <Field
-                      type='text'
+                      type={attribute.key_type}
                       name={attribute.id}
                       required={attribute.required}
-                      value={prop(attribute.id, values)}
                     />
+                    <div className='form--error'>
+                      <ErrorMessage name={attribute.id} />
+                    </div>
                   </div>
                 })
                   : 'No profile form to fill yet'
