efbe97e3b2e3525d8d32da23c332e4f4a0661136
Marc Farra <marcfarra@gmail.com>
2022-05-06 17:31:41 +0300

    Fix uh oh message

--- pages/uh-oh.js
+++ pages/uh-oh.js
@@ -6,6 +6,8 @@ export default class UhOh extends Component {
       <article className='inner page'>
         <h1>Page not found</h1>
         <div>Sorry, the page you are looking for is not available.</div>
+        <br />
+        <div>Still having problems? Try logging out and back in or contacting a system administrator.</div>
       </article>
     )
   }
