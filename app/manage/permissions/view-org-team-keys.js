f06ebc8a02a81fa31e42a31401fd525578669034
Marc Farra <marcfarra@gmail.com>
2022-11-09 16:10:37 +0200

    change permission for orgTeamAttributes call

---
+++ app/manage/permissions/view-org-team-keys.js
@@ -0,0 +1,19 @@
+const { isOwner, isOrgTeamModerator } = require('../../lib/organization')
+
+/**
+ * organization:view-team-keys
+ *
+ * To edit an organization or delete it, the authenticated user needs
+ * to be an owner in the organization
+ *
+ * @param {int} uid - user id
+ * @param {Object} params - request parameters
+ * @param {int} params.id - organization id
+ * @returns {Promise<boolean>}
+ */
+async function editOrg (uid, { id }) {
+  const teamModerator = await isOrgTeamModerator(id, uid)
+  return teamModerator || isOwner(id, uid)
+}
+
+module.exports = editOrg
