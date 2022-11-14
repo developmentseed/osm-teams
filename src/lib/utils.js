07f4d0be9909c20401602717ad1c11e264a28cee
Vitor George <vitor.george@gmail.com>
2022-11-14 18:46:32 +0000

    Fix badge create/edit

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,4 @@
-function getRandomColor() {
+export function getRandomColor() {
   var letters = '0123456789ABCDEF'
   var color = '#'
   for (var i = 0; i < 6; i++) {
@@ -7,6 +7,13 @@ function getRandomColor() {
   return color
 }
 
-module.exports = {
-  getRandomColor,
+/**
+ * Converts a date to the browser locale string
+ *
+ * @param {Number or String} timestamp
+ * @returns
+ */
+export function toDateString(timestamp) {
+  const dateFormat = new Intl.DateTimeFormat(navigator.language).format
+  return dateFormat(new Date(timestamp))
 }
