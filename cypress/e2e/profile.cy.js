2623421a336303d5aa03f4f54dcb784100caa8ef
Vitor George <vitor.george@gmail.com>
2022-11-18 15:13:04 +0000

    Add next-auth middleware, protect profile page

--- cypress/e2e/profile.cy.js
+++ cypress/e2e/profile.cy.js
@@ -4,8 +4,14 @@ describe('Profile page', () => {
     display_name: 'User 1',
   }
 
+  it('Redirect to sign in when not logged in', () => {
+    cy.visit('/profile')
+    cy.get('body').should('contain', 'Sign in')
+  })
+
   it('Can load when logged in', () => {
     cy.login(user)
     cy.visit('/profile')
+    cy.get('body').should('not.contain', 'Sign in')
   })
 })
