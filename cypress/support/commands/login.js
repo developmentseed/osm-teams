9bc6196fd1b0854cd0dbfe7202e15578af9dd6a5
Vitor George <vitor.george@gmail.com>
2022-12-07 09:51:30 +0000

    Update getSessionToken function

--- cypress/support/commands/login.js
+++ cypress/support/commands/login.js
@@ -1,13 +1,10 @@
 const getSessionToken = require('../../../tests/utils/get-session-token')
 
-Cypress.Commands.add('login', (userObj) => {
+Cypress.Commands.add('login', (user) => {
   // Generate and set a valid cookie from the fixture that next-auth can decrypt
   cy.wrap(null)
     .then(() => {
-      return getSessionToken(
-        { ...userObj, userId: userObj.id },
-        Cypress.env('NEXTAUTH_SECRET')
-      )
+      return getSessionToken(user, Cypress.env('NEXTAUTH_SECRET'))
     })
     .then((encryptedToken) =>
       cy.setCookie('next-auth.session-token', encryptedToken)
