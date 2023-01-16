469def3326900bc580e558c76f2f475455c7713c
Marc Farra <marcfarra@gmail.com>
2023-01-16 16:40:01 +0200

    try capturing inside the no match

--- src/middlewares/base-handler.js
+++ src/middlewares/base-handler.js
@@ -48,6 +48,11 @@ export function createBaseHandler() {
       })
     },
     onNoMatch: (req, res) => {
+      if (req.method === 'OPTIONS') {
+        logger.info('OPTIONS request')
+        res.send(200).end()
+      }
+
       res.status(404).json({
         statusCode: 404,
         error: 'Not Found',
