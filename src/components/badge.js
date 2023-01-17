03755be05ce7f04c5ea03b3b54e63d09ba65d361
Lane Goodman <Lane@developmentseed.org>
2023-01-17 12:54:16 -0500

    Limit badges column to 3 badges in users table

--- src/components/badge.js
+++ src/components/badge.js
@@ -12,9 +12,12 @@ const hexToRgb = (hex) =>
     .map((x) => parseInt(x, 16))
     .join()
 
-export default function Badge({ name, color, children }) {
+export default function Badge({ name, color, dot, children }) {
+  let classes = [`badge`]
+  if (dot) classes.push('dot')
+  let classNames = classes.join(' ')
   return (
-    <span className='badge'>
+    <span className={classNames}>
       {children}
       {name}
       <style jsx>
@@ -30,10 +33,11 @@ export default function Badge({ name, color, children }) {
             font-size: 12px;
             white-space: nowrap;
             margin-right: 4px;
+            line-height: initial;
             position: relative;
             overflow: hidden;
           }
-          .badge::before {
+          .badge.dot::before {
             content: '';
             background-color: ${color};
             box-shadow: 0 0 0 1px ${theme.colors.primaryLite};
