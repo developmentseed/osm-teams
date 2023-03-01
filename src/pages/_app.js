c4f1f6efa152681094ff79c8b8eed8a60c8c81d3
Lane Goodman <Lane@developmentseed.org>
2023-02-28 22:26:11 -0500

    Use page header in place of sidebar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,52 +1,19 @@
 import React from 'react'
-import {
-  ChakraProvider,
-  withDefaultColorScheme,
-  extendTheme,
-} from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
+import '@fontsource/work-sans/400.css'
+import '@fontsource/work-sans/700.css'
+import '@fontsource/inconsolata/400.css'
+import '@fontsource/inconsolata/700.css'
 
 import Head from 'next/head'
-import Sidebar from '../components/sidebar'
 import Layout from '../components/layout.js'
 import { ToastContainer } from 'react-toastify'
 import { SessionProvider } from 'next-auth/react'
 import join from 'url-join'
+import theme from '../styles/theme'
+import PageHeader from '../components/page-header'
 
 const APP_URL = process.env.APP_URL
-const colors = {
-  brand: {
-    50: '#ECEEF8',
-    100: '#CAD0EC',
-    200: '#A8B2E0',
-    300: '#8794D4',
-    400: '#6576C8',
-    500: '#4358BC',
-    600: '#354797',
-    700: '#283571',
-    800: '#1B234B',
-    900: '#0D1226',
-  },
-}
-
-const components = {
-  Button: {
-    variants: {
-      solid: {
-        borderRadius: 0,
-        textTransform: 'uppercase',
-        _hover: {
-          bg: 'brand.600',
-          color: '#FFFFFF',
-        },
-      },
-    },
-  },
-}
-
-const theme = extendTheme(
-  { colors, components },
-  withDefaultColorScheme({ colorScheme: 'brand' })
-)
 
 export default function App({
   Component,
@@ -64,7 +31,7 @@ export default function App({
           <meta charSet='utf-8f-8' />
         </Head>
         <Layout>
-          <Sidebar />
+          <PageHeader />
           <Component {...pageProps} />
         </Layout>
         <ToastContainer position='bottom-right' />
