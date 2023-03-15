bdd5f0fa51293023693ac34293ceaa7e7900613b
Lane Goodman <Lane@developmentseed.org>
2023-03-15 15:06:45 -0400

    Style 404 page

--- src/pages/uh-oh.js
+++ src/pages/uh-oh.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react'
+import { Box, Container, Heading, Text } from '@chakra-ui/react'
+import InpageHeader from '../components/inpage-header'
 
 export default class UhOh extends Component {
   render() {
     return (
-      <article className='inner page'>
-        <h1>Page not found</h1>
-        <div>Sorry, the page you are looking for is not available.</div>
-        <br />
-        <div>
-          Still having problems? Try logging out and back in or contacting a
-          system administrator.
-        </div>
-      </article>
+      <Box as='main' mb={8}>
+        <InpageHeader>
+          <Heading color='white' mb={2}>
+            Page Not Found
+          </Heading>
+        </InpageHeader>
+        <Container maxW='container.xl' as='section'>
+          <Box layerStyle={'shadowed'}>
+            <Text fontSize='2xl'>
+              Sorry, the page you are looking for is not available.
+            </Text>
+            <Text>
+              Still having problems? Try logging out and back in or contacting a
+              system administrator.
+            </Text>
+          </Box>
+        </Container>
+      </Box>
     )
   }
 }
