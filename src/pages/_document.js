495d1765a8c06a0650f6d0db39682524c1be68a3
Lane Goodman <Lane@developmentseed.org>
2023-04-06 21:53:10 -0400

    Update favicons and logo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -30,6 +30,24 @@ export default function Document() {
           type='image/png'
           href={join(APP_URL, '/static/favicon.png')}
         />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href={join(APP_URL, '/static/apple-touch-icon.png')}
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href={join(APP_URL, '/static/favicon-32x32.png')}
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href={join(APP_URL, '/static/favicon-16x16.png')}
+        />
+        <link rel='manifest' href={join(APP_URL, '/static/site.webmanifest')} />
       </Head>
       <body>
         <Main />
